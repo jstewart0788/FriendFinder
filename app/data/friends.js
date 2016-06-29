07fcfaf0e970a609fb1002cf13dd9edbeca79851
Jonathan Stewart <AnorLondo@Jonathans-MacBook-Air.local>
2016-06-29 02:32:49 -0400

    Website functionality implemented

---
+++ app/data/friends.js
@@ -0,0 +1,70 @@
+//data
+var  friendData =
+[
+	{
+	  "name":"Darth Vader",
+	  "photo":"http://digitalspyuk.cdnds.net/16/04/768x384/landscape-1453994837-movies-star-wars-darth-vader.jpg",
+	  "scores":[
+	     3,
+	     1,
+	     5,
+	     3,
+	     2,
+	     1,
+	     4,
+	     5,
+	     3,
+	     2
+	   ]
+	},
+	{
+	  "name":"Carnor Jax",
+	  "photo":"http://img2.wikia.nocookie.net/__cb20090908033324/starwars/images/1/1d/SWCE1-FC.jpg",
+	  "scores":[
+	     1,
+	     5,
+	     3,
+	     1,
+	     2,
+	     3,
+	     2,
+	     3,
+	     4,
+	     5
+	   ]
+	},
+	{
+	  "name":"Captain Yorr",
+	  "photo":"http://vignette3.wikia.nocookie.net/starwars/images/8/8a/Captain_yorr.jpg/revision/latest?cb=20071002224249",
+	  "scores":[
+	     3,
+	     2,
+	     5,
+	     2,
+	     1,
+	     1,
+	     2,
+	     1,
+	     2,
+	     3
+	   ]
+	},
+	{
+	  "name":"Soontir Fel",
+	  "photo":"http://vignette1.wikia.nocookie.net/starwars/images/1/1f/The_Fel.jpg/revision/latest?cb=20071125063408",
+	  "scores":[
+	     5,
+	     5,
+	     4,
+	     4,
+	     3,
+	     2,
+	     4,
+	     3,
+	     2,
+	     5
+	   ]
+	}
+];
+
+module.exports = friendData;
